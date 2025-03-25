851f67ba44dd4a6a18e4cdace777b7073b5f3eaa
alex-schein-NOAA <alex.schein@noaa.gov>
2025-03-25 15:57:13 -0600

    Regridding failure
    Need to experiment with wgrib2 utility on HERA

--- functions_processing.py
+++ functions_processing.py
@@ -1,61 +1,113 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 21 15:49:36 2025
-
-@author: alex.schein
-"""
-
 from header_processing import *
 
 
-def FindIndicesOfSouthwestCornerOfCO_HRRR(MIN_LAT, MIN_LON):
-    #Finds the indices of the SW corner of CO
-    #Expensive and should only be run once!
-    #Designed to be self-contained apart from coords of SW corner
+def FindIndicesOfSpecificPoint(LAT=37, LON=251, model='hrrr'): #default coords = SW corner of CO
     #lat and lon are ordered to where north and east are positive, respectively
-    #thus to find the SW corner of CO that we want to build from, start from the min of both
-    
-    
-    #adapted from https://github.com/blaylockbk/pyBKB_v3/blob/master/demo/Nearest_lat-lon_Grid.ipynb 
-    
-    
-    #random HRRR dataset - coords same between all so specific doesn't matter
-    H = Herbie("2014-08-02 00:00", model="hrrr", product='sfc', verbose=False)
-    HRRR_t2m = H.xarray(r":TMP:surface")
-    
-    abslat = np.abs(HRRR_t2m.latitude.data - MIN_LAT)
-    abslon = np.abs(HRRR_t2m.longitude.data - MIN_LON)
+    #adapted from https://github.com/blaylockbk/pyBKB_v3/blob/master/demo/Nearest_lat-lon_Grid.ipynb     
 
-    center = np.maximum(abslat, abslon)
-
-    y_sw, x_sw = np.where(center==np.min(center))
-    
-    #returns the raw integers of the indices, NOT the xarray or int64
-    #ORDER GIVES LON THEN LAT 
-    return int(x_sw[0]), int(y_sw[0])
-
-
-def FindIndicesOfSpecificPoint(MIN_LAT=37, MIN_LON=251, model='hrrr'):
-    
     # designed to work in both HRRR and URMA grids
-    if model=='hrrr':
+    if type(model) is xr.core.dataarray.DataArray: #OVERLOADING: pass URMA dataset (e.g. t2m) in for "model" for this
+        ds = model
+    elif model=='hrrr':
         H = Herbie("2014-08-02 00:00", model="hrrr", product='sfc', verbose=False)
         ds = H.xarray(r":TMP:surface")
     elif model=='urma':
         H = Herbie("2024-01-01 00:00", model="urma", verbose=False)
-        H.download()
-        ds = H.xarray()[3]
-        ds = ds['t2m']
+        urma = H.download()
+        ds = SelectURMAt2m(urma)
     else:
-        print("This method only works for HRRR and URMA data. Enter ' model='hrrr' or ' model='urma' ' ")
+        print("This method only works for HRRR and URMA data. Enter ' model='hrrr' ' or ' model='urma' ' or pass in an URMA DataArray")
         
-    abslat = np.abs(ds.latitude.data - MIN_LAT)
-    abslon = np.abs(ds.longitude.data - MIN_LON)
+    abslat = np.abs(ds.latitude.data - LAT)
+    abslon = np.abs(ds.longitude.data - LON)
     
     center = np.maximum(abslat, abslon)
 
-    y_sw, x_sw = np.where(center==np.min(center))
+    y_c, x_c = np.where(center==np.min(center))
     
     #returns the raw integers of the indices, NOT the xarray or int64
     #ORDER GIVES LON THEN LAT 
-    return int(x_sw[0]), int(y_sw[0])
+    return int(x_c[0]), int(y_c[0])
+
+
+def SelectURMAt2m(urma):
+    #input: list of hypercubes from URMA download via Herbie
+    #horrible but needed to account for different dataset structures in different URMA sets
+    flag = True
+    HYPERCUBE_LOC=0
+    while flag:
+        try:
+            urma_t2m = urma[HYPERCUBE_LOC]['t2m']
+            flag=False #should only run if previous line is a success
+            print(f"t2m found in hypercube {HYPERCUBE_LOC}")
+        except:
+            HYPERCUBE_LOC+=1
+            if HYPERCUBE_LOC>10:
+                flag=False
+                print("Something went wrong when trying to locate t2m")
+    
+    return urma_t2m
+
+
+def GenerateNDFDCoords():
+    
+    print("Generating NDFD coordinate NetCDF")
+    
+    ds = xr.open_dataset(r'C:\Users\alex.schein\Test code and files\Test files\NDFD_example.grib2', engine='cfgrib')
+
+    NDFD_lats = ds.latitude.data
+    NDFD_lons = ds.longitude.data
+
+    with Dataset(r'C:\Users\alex.schein\Test code and files\Test files\NDFD_coords.nc', 'w') as f:
+        f.createDimension('x', np.shape(NDFD_lons)[1])
+        f.createDimension('y', np.shape(NDFD_lons)[0])
+        
+        lonvar = f.createVariable('lons_NDFD', 'float32', ('y','x'))
+        lonvar[:] = NDFD_lons
+        
+        latvar = f.createVariable('lats_NDFD', 'float32', ('y','x'))
+        latvar[:] = NDFD_lats
+        
+    return
+
+####################################
+# TO DO: make master function that calls the 4 below and returns the indices to trim HRRR, URMA so we get a shared domain for regridding
+
+def IsNorthOfSouthernmostRow(HRRR_t2m, URMA_t2m):
+    #input: HRRR, URMA t2m DataArrays
+    #output: vector of bools w/ len = # rows in URMA, same ordering, w/ True if row is COMPLETELY north of southernmost row of lats in HRRR
+    
+    dataset1 = URMA_t2m.latitude.data
+    dataset2 = HRRR_t2m.latitude.data
+    
+    max_lat_ds2 = np.max(dataset2[0])
+    output = np.zeros(np.shape(dataset1)[0])
+    for i in range(np.shape(dataset1)[0]):
+        #is min lat in a row of URMA below the max lat of the southernmost row of HRRR?
+        min_lat_ds1 = np.min(dataset1[i])
+        if min_lat_ds1 >= max_lat_ds2:
+            output[i] = True
+        else:
+            output[i] = False
+            
+    return output
+
+
+def IsEastOfWesternmostColumn(HRRR_t2m, URMA_t2m):
+    #input: HRRR, URMA t2m DataArrays
+    #output: vector of bools w/ len = # columns in HRR, same ordering, w/ True if column is COMPLETELY east of westernmost column of lats in HRRR
+    
+    dataset2 = URMA_t2m.longitude.data
+    dataset1 = HRRR_t2m.longitude.data
+    
+    max_lon_ds2 = np.max(dataset2[:,0])
+    output = np.zeros(np.shape(dataset1)[1])
+    for i in range(np.shape(dataset1)[1]):
+        #is min lon in a column of HRRR east of the max lon of the westernmost column of URMA?
+        min_lon_ds1 = np.max(dataset1[:,i])
+        if min_lon_ds1 >= max_lon_ds2:
+            output[i] = True
+        else:
+            output[i] = False
+    
+    return output
--- regridding_testing_HRRR.py
+++ regridding_testing_HRRR.py
@@ -6,115 +6,136 @@ import time
 #%% Generate NDFD coords
 # !!!!!!!!!!!! SHOULD NOT HAVE TO BE RUN AGAIN, IF NDFD_coords.nc IS ON DISK !!!!!!!!
 
-# ds = xr.open_dataset(r'C:\Users\alex.schein\Test code and files\Test files\NDFD_example.grib2', engine='cfgrib')
-
-# NDFD_lats = ds.latitude.data
-# NDFD_lons = ds.longitude.data
-
-# with Dataset(r'C:\Users\alex.schein\Test code and files\Test files\NDFD_coords.nc', 'w') as f:
-#     f.createDimension('x', np.shape(NDFD_lons)[1])
-#     f.createDimension('y', np.shape(NDFD_lons)[0])
-    
-#     lonvar = f.createVariable('lons_NDFD', 'float32', ('y','x'))
-#     lonvar[:] = NDFD_lons
-    
-#     latvar = f.createVariable('lats_NDFD', 'float32', ('y','x'))
-#     latvar[:] = NDFD_lats
+# GenerateNDFDCoords()
 
 #%% Global vars
 
 PATH_TO_DOWNLOAD_TO = r"C:\Users\alex.schein\Test code and files\Test files\Herbie_downloads"
 
-#bounds of rectangular subregion of interest, in degrees
-# MIN_LAT = 37 
-# MIN_LON = 360-109
+#%% Dataset DLs
 
-# MAX_LAT = 41
-# MAX_LON = 360-104
-
-#%% 
-
-# testing on one HRRR file
 H_HRRR = Herbie("2014-08-02 00:00", model="hrrr", product='sfc', save_dir=PATH_TO_DOWNLOAD_TO, verbose=True)
 HRRR_t2m = H_HRRR.xarray(r":TMP:surface") #does NOT download to local directory!
 
+# ds_coords = xr.open_dataset(r"C:\Users\alex.schein\Test code and files\Test files\NDFD_coords.nc")
+# ds_coords = ds_coords.rename({"lons_NDFD":"lon", "lats_NDFD":"lat"}) #to be safe
 
-ds_coords = xr.open_dataset(r"C:\Users\alex.schein\Test code and files\Test files\NDFD_coords.nc")
-ds_coords = ds_coords.rename({"lons_NDFD":"lon", "lats_NDFD":"lat"}) #to be safe
+H = Herbie("2014-08-02 00:00", model='urma', save_dir=PATH_TO_DOWNLOAD_TO, verbose=False)
+H.download()
+URMA_t2m = SelectURMAt2m(H.xarray())
 
+
+#%% 
+
+# lats_HRRR = HRRR_t2m.t.latitude.data
+# lats_NDFD = ds_coords.lat.data
+# lats_urma = urma_t2m.latitude.data
+
+# lons_HRRR = HRRR_t2m.t.longitude.data
+# lons_NDFD = ds_coords.lon.data
+# lons_urma = urma_t2m.longitude.data
+
+##### URMA and NDFD grids align but URMA extends further north - also need to deal with the slightly different grid of 2024 data - investigate...
+
+#%%
+
+#routine to check the following:
+    # 1. if a row of lats of dataset1 is completely north of the southernmost row of lats of dataset2
+    # 2. if a column of lons of dataset1 is completely east of the westernmost column of lons of dataset2
+    # 3. if a row of lats of dataset1 is completely south of the northernmost row of lats of dataset2
+    # 4. if a column of lons of dataset1 is completely west of the easternmost column of lats of dataset2
+    
+# As HRRR is less tall but wider than URMA, should have the following ordering:
+    # 1. dataset1 = URMA, dataset2 = HRRR
+    # 2. dataset1 = HRRR, dataset2 = URMA
+    # 3. dataset1 = URMA, dataset2 = HRRR
+    # 4. dataset1 = HRRR, dataset2 = URMA
+    
+   
+def IsSouthOfNorthernmostRow(HRRR_t2m, URMA_t2m):
+    #input: HRRR, URMA t2m DataArrays
+    #output: vector of bools w/ len = # rows in URMA, same ordering, w/ True if row is COMPLETELY north of southernmost row of lats in HRRR
+    
+    dataset1 = URMA_t2m.latitude.data
+    dataset2 = HRRR_t2m.latitude.data
+
+#%% testing each func
+
+dataset1 = URMA_t2m.latitude.data
+dataset2 = HRRR_t2m.latitude.data
+
+#%%
+
+min_lat_ds2 = np.min(dataset2[-1])
+output = np.zeros(np.shape(dataset1)[0])
+for i in range(np.shape(dataset1)[0]):
+    #is max lat in a row of URMA below the min lat of the northernmost row of HRRR?
+    max_lat_ds1 = np.max(dataset1[i])
+    if min_lat_ds1 >= max_lat_ds2:
+        output[i] = True
+    else:
+        output[i] = False
+
+
+#%%
+
+test_URMA = URMA_t2m.where((URMA_t2m.latitude >= np.min(HRRR_t2m.latitude.data[0]) ), drop=True)
 
 #%% 
 # SLOW TO BUILD - DON'T REDO
 
-start_cpu1 = time.process_time()
-start_wall1 = time.time()
-regridder_bilinear = xesmf.Regridder(HRRR_t2m, ds_coords, 'bilinear')
-end_cpu1 = time.process_time() - start_cpu1
-end_wall1 = time.time() - start_wall1
-print(f"Bilinear regridder done. Build time = {end_cpu1} s cpu, {end_wall1} s wall")
+# start_cpu1 = time.process_time()
+# start_wall1 = time.time()
+# regridder_bilinear = xesmf.Regridder(HRRR_t2m, ds_coords, 'bilinear')
+# end_cpu1 = time.process_time() - start_cpu1
+# end_wall1 = time.time() - start_wall1
+# print(f"Bilinear regridder done. Build time = {end_cpu1} s cpu, {end_wall1} s wall")
 
 
-start_cpu2 = time.process_time()
-start_wall2 = time.time()
-regridder_patch = xesmf.Regridder(HRRR_t2m, ds_coords, 'patch')
-end_cpu2 = time.process_time() - start_cpu2
-end_wall2 = time.time() - start_wall2
-print(f"Patch regridder done. Build time = {end_cpu2} s cpu, {end_wall2} s wall")
+# start_cpu2 = time.process_time()
+# start_wall2 = time.time()
+# regridder_patch = xesmf.Regridder(HRRR_t2m, ds_coords, 'patch')
+# end_cpu2 = time.process_time() - start_cpu2
+# end_wall2 = time.time() - start_wall2
+# print(f"Patch regridder done. Build time = {end_cpu2} s cpu, {end_wall2} s wall")
 
 
-regridder_bilinear.to_netcdf(r"C:\Users\alex.schein\Test code and files\Test files\regridder_bilinear_HRRRtoNDFD.nc")
-regridder_patch.to_netcdf(r"C:\Users\alex.schein\Test code and files\Test files\regridder_patch_HRRRtoNDFD.nc")
-
-
-#%%
+# regridder_bilinear.to_netcdf(r"C:\Users\alex.schein\Test code and files\Test files\regridder_bilinear_HRRRtoNDFD.nc")
+# regridder_patch.to_netcdf(r"C:\Users\alex.schein\Test code and files\Test files\regridder_patch_HRRRtoNDFD.nc")
 
 
 #Fast but doesn't give rectuangular data array........
 
-HRRR_t2m_regridded_bilinear = regridder_bilinear(HRRR_t2m)
-HRRR_t2m_regridded_patch = regridder_patch(HRRR_t2m)
-
-#%% 
-
-# testing on one URMA file
-H = Herbie("2014-08-02 00:00", model='urma', save_dir=PATH_TO_DOWNLOAD_TO, verbose=False)
-H.download()
-urma = H.xarray()
-
-#horrible but needed to account for different dataset structures in different URMA sets
-flag = True
-HYPERCUBE_LOC=0
-while flag:
-    try:
-        urma_t2m = urma[HYPERCUBE_LOC]['t2m']
-        flag=False #should only run if previous line is a success
-        print(f"t2m found in hypercube {HYPERCUBE_LOC}")
-    except:
-        HYPERCUBE_LOC+=1
-        if HYPERCUBE_LOC>10:
-            flag=False
-            print("Something went wrong when trying to locate t2m")
-
-##### URMA and NDFD grids align but URMA extends further north - also need to deal with the slightly different grid of 2024 data - investigate...
+# HRRR_t2m_regridded_bilinear = regridder_bilinear(HRRR_t2m)
+# HRRR_t2m_regridded_patch = regridder_patch(HRRR_t2m)
 
 
 #%%
 
-# projection = ccrs.LambertConformal(
-#     central_longitude = -87.5,
-#     central_latitude = 38.5,
-#     standard_parallels = (38.5,38.5),
-#     cutoff = 0)
+projection = ccrs.LambertConformal(
+    central_longitude = 265-360,
+    central_latitude = 38.5,
+    standard_parallels = (38.5,38.5),
+    cutoff = 0)
 
-# fig = plt.figure()#figsize=(12,9))
-# ax1 = fig.add_subplot(1,1,1, projection=projection)
+fig = plt.figure()#figsize=(12,9))
+ax1 = fig.add_subplot(1,1,1, projection=projection)
 # ax2 = fig.add_subplot(1,2,2, projection=projection)
 
 # ax1.set_extent([-114, -98, 32, 45])
-# ax1.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth=2)
-# ax1.add_feature(cfeature.STATES.with_scale('50m'))
-# ax1.contourf(HRRR_t2m.longitude.data, HRRR_t2m.latitude.data, HRRR_t2m.t.data, transform=ccrs.PlateCarree(), cmap=cm.coolwarm)
-# ax1.set_title("xarray.where selection")
+ax1.set_extent([236-360, 292-360, 19, 24])
+ax1.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth=1)
+ax1.add_feature(cfeature.STATES.with_scale('50m'))
+ax1.scatter(URMA_t2m.longitude.data, URMA_t2m.latitude.data, transform=ccrs.PlateCarree(), s=0.02, c='r', marker='X', label='URMA')
+ax1.scatter(HRRR_t2m.longitude.data, HRRR_t2m.latitude.data, transform=ccrs.PlateCarree(), s=0.02, c='k', marker ='X', label='HRRR')
+# ax1.scatter(URMA_t2m.longitude.data[186:,:], URMA_t2m.latitude.data[186:,:], transform=ccrs.PlateCarree(), s=0.02, c='r', marker='X', label='URMA')
+gl1 = ax1.gridlines(draw_labels=True)
+gl1.top_labels=False
+gl1.bottom_labels=False
+gl1.right_labels=False
+
+# ax1.legend(loc='center left')
+ax1.set_title("Misalignment of HRRR and URMA grids, full southern region")
 
 # ax2.set_extent([-114, -98, 32, 45])
 # ax2.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth=2)
@@ -122,4 +143,4 @@ while flag:
 # ax2.contourf(subregion_idx.longitude.data, subregion_idx.latitude.data, subregion_idx.t.data, transform=ccrs.PlateCarree(), cmap=cm.coolwarm)
 # ax2.set_title("index-based selection")
 
-# plt.savefig(r'C:\Users\alex.schein\Test code and files\selections.png', dpi=300, bbox_inches='tight')
+plt.savefig(r'C:\Users\alex.schein\Test code and files\grid_misalignment_south.png', dpi=300, bbox_inches='tight')
